4653901efb27b28683e00239ff3474597f5f2e73
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-10-23 08:21:11 +0700

    menambahkan function ActivitiesPertama dan layout column beserta komponen text pertamanya

--- app/src/main/java/com/example/praktikum4_234/MainActivity.kt
+++ app/src/main/java/com/example/praktikum4_234/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.praktikum4_234.ui.theme.Praktikum4_234Theme
 
@@ -31,17 +34,10 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Praktikum4_234Theme {
-        Greeting("Android")
+fun ActivitiesPertama(modifier: Modifier){
+    Column {
+        Text(text = stringResource(id = R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold)
     }
 }

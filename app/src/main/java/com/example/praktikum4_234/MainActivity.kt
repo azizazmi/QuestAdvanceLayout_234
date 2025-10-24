b99c2e0342af78942258bdd03d71f65bc22b9fdb
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-10-24 20:19:52 +0700

    feat: menambahkan komponen text untuk nomor telepon dalam layout column

--- app/src/main/java/com/example/praktikum4_234/MainActivity.kt
+++ app/src/main/java/com/example/praktikum4_234/MainActivity.kt
@@ -134,7 +134,12 @@ fun ActivitiesPertama(modifier: Modifier){
                         color = Color.Cyan,
                         modifier = Modifier.padding(top = 8.dp)
                     )
-
+                    Text(
+                        text = stringResource(id = R.string.notelp),
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 8.dp)
+                    )
             }
         }
         Box(
